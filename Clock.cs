82fc2c56711fb7a476c6b04f61b4a210fc6cebf4
BogdanOparlescu <bogdanoparlescu@yahoo.com>
2024-05-15 21:04:22 +0300

    Basic class structure

---
+++ Clock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoobyDoo
+{
+    internal class Clock
+    {
+        //time in ms when the clock was created
+        private int create_time;
+        public Clock() 
+        { 
+            //set create time
+        }
+
+        public int GetLapTime()
+        {
+            //time right now - create time
+            return 0;
+        }
+    }
+}
---
+++ Threading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoobyDoo
+{
+    internal class Threading
+    {
+        public int[] GenerateRandomArray(int length)
+        {
+            //To Do
+            return new int[length];
+        }
+
+        void ThreadedArraySum(int[] array, int no_threads)
+        {
+            //divide the array into parts
+            //create a thread for each part
+            //for each thread => sum the array on that division
+            //return sum(all_thread_results)
+        }
+
+        //The same as the function above, but modulo <mod>
+        void ThreadedArraySumMod(int[] array, int no_threads,int mod)
+        {
+
+        }
+
+
+        void ThreadedArrayAverage(int[] array, int no_threads)
+        {
+            //average for each thread result
+            //average of the averaged_thread_results
+        }
+    }
+}
